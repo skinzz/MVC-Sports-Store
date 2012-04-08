510caf7d270d7dc269d5d99ae2582fe33379e94f
Will Sanders <will@clickatwill.com>
2012-04-08 01:00:24 -0400

    Added Test for list, pagination, Started Total pages helper

---
+++ SportsStore.UnitTests/ProductControllerTest.cs
@@ -0,0 +1,92 @@
+﻿using SportsStore.WebUI.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting.Web;
+using SportsStore.Domain.Abstract;
+using System.Web.Mvc;
+
+namespace SportsStore.UnitTests
+{
+    
+    
+    /// <summary>
+    ///This is a test class for ProductControllerTest and is intended
+    ///to contain all ProductControllerTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class ProductControllerTest
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        /// <summary>
+        ///A test for List
+        ///</summary>
+        // TODO: Ensure that the UrlToTest attribute specifies a URL to an ASP.NET page (for example,
+        // http://.../Default.aspx). This is necessary for the unit test to be executed on the web server,
+        // whether you are testing a page, web service, or a WCF service.
+        [TestMethod()]
+        [HostType("ASP.NET")]
+        [AspNetDevelopmentServerHost("C:\\CodeProj\\SportsStore\\SportsStore.WebUI", "/")]
+        [UrlToTest("http://localhost:49276/")]
+        public void ListTest()
+        {
+            IProductRepository productRepository = null; // TODO: Initialize to an appropriate value
+            ProductController target = new ProductController(productRepository); // TODO: Initialize to an appropriate value
+            int page = 0; // TODO: Initialize to an appropriate value
+            ViewResult expected = null; // TODO: Initialize to an appropriate value
+            ViewResult actual;
+            actual = target.List(page);
+            Assert.AreEqual(expected, actual);
+            Assert.Inconclusive("Verify the correctness of this test method.");
+        }
+    }
+}
